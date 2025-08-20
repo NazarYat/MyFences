da35d52dce15ff716aea4911de9f33a0bfe0c892
Nazar Yatsyuk <yatsyuk.nazar@gmail.com>
2025-08-20 06:15:33 +0300

    Fixed setup

--- MyFences/Scripts/UnregisterAutoStart.ps1
+++ MyFences/Scripts/UnregisterAutoStart.ps1
@@ -1,1 +1,8 @@
-﻿Unregister-ScheduledTask -TaskName 'MyFences' -Confirm:$false
+﻿$taskName = "MyFences"
+
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+    Write-Output "Task '$taskName' was found and removed."
+} else {
+    Write-Output "Task '$taskName' does not exist."
+}
